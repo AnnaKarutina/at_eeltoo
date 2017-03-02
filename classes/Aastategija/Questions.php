9400a30d197f52aebbf36762e4006ed7520df01d
reneesaks <renee.saks@khk.ee>
2017-03-02 08:11:08 +0200

    CSS bug fix

--- classes/Aastategija/Questions.php
+++ classes/Aastategija/Questions.php
@@ -15,14 +15,18 @@ class Questions
     static function get() {
         q('SELECT * FROM questions JOIN answers USING (question_id) ORDER BY RAND()', $q);
         while ($row = mysqli_fetch_assoc($q)) {
-            $questions[$row['question_id']]['question'] = $row['question'];
+            $questions[$row['question_id']]['question'] = htmlentities($row['question']);
             $questions[$row['question_id']]['question_id'] = $row['question_id'];
             $questions[$row['question_id']]['answers'][] = [
                 'id' => $row['answer_id'],
                 'text' => htmlentities($row['answer_text'])
             ];
         }
-
         return array_slice($questions, 0, 10);
+    }
+
+    static function get_practical() {
+        $practicalText = get_first('SELECT practical_text FROM practical ORDER BY RAND()');
+        return explode(';', $practicalText['practical_text'], -1);
     }
 }
---
+++ views/practical/practical_index.php
@@ -0,0 +1,33 @@
+<form action="test/confirm" method="POST" id="quiz">
+    <?php foreach ($questions as $question): ?>
+        <article>
+            <p><?= $question['question'] ?></p>
+            <ul>
+                <?php foreach ($question['answers'] as $answer): ?>
+                    <li>
+                        <label>
+                            <input type="radio" value="<?= $answer['id'] ?>"
+                                   name="answers[<?= $question['question_id'] ?>]"> <?= $answer['text'] ?>
+                        </label
+                    </li>
+                <?php endforeach ?>
+            </ul>
+        </article>
+    <?php endforeach ?>
+    <a href="#" id="submit-quiz" class="btn btn-info btn-lg form-button" data-toggle="modal" data-target=".confirm">Esita</a>
+</form>
+
+<div class="modal fade confirm">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Oled sa kindel, et soovid esitada lahendust?</h4>
+                <h4 id="checked"></h4>
+            </div>
+            <div class="modal-footer">
+                <button id = "yes" type="button" class="btn btn-default" data-dismiss="modal">Jah</button>
+                <button id = "no" type="button" class="btn btn-primary">Ei</button>
+            </div>
+        </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+</div><!-- /.modal -->
