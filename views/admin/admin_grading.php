2ecb4cd39f7ab9dd2c466376fc40f7cb6f6275bc
reneesaks <renee.saks@khk.ee>
2017-03-13 09:06:26 +0200

    Fixed bugs

--- views/admin/admin_grading.php
+++ views/admin/admin_grading.php
@@ -20,7 +20,7 @@
                             <?php if ($result['practical_points'] == -2): ?>
                                 <span class="not-graded">Tegemata</span>
                             <?php elseif ($result['practical_points'] == -1): ?>
-                                <span class="not-graded">Hindamata</span>
+                                <span class="not-graded-<?= $result['user_id'] ?> not-graded">Hindamata</span>
                             <?php else: ?>
                                 <span class="graded">Hinnatud: </span>
                                 <span class="graded"
@@ -39,102 +39,105 @@
         <div class="tab-content">
 
             <?php foreach ($results as $result): ?>
-            <?php if ($result['practical_points'] != -2): ?>
-                <div class="tab-pane fade" id="user-<?= $result['user_id'] ?>">
-                    <h4>
-                        <?=
-                        $result['firstname'] . ' ' .
-                        $result['lastname'] . ', ' .
-                        $result['social_id'] . ', ' .
-                        date("d.m.Y", strtotime($result['date']));
-                        ?>
-                    </h4>
+                <?php if ($result['practical_points'] != -2): ?>
+                    <div class="tab-pane fade" id="user-<?= $result['user_id'] ?>">
+                        <h4>
+                            <?=
+                            $result['firstname'] . ' ' .
+                            $result['lastname'] . ', ' .
+                            $result['social_id'] . ', ' .
+                            date("d.m.Y", strtotime($result['date']));
+                            ?>
+                        </h4>
 
-                    <?php if(file_exists('results/'.$result['social_id'].'.html')): ?>
-                    <button id="view-<?= $result['user_id'] ?>" class="preview"
-                            data-target="#modal-<?= $result['user_id'] ?>"
-                            data-toggle="modal">
-                        Eelvaade
-                    </button>
-                    <a href="results/<?= $result['social_id'] ?>.html" target="_blank">Link</a>
-                    <br>
-                    <br>
-                    <?php else: ?>
-                    <h5>Antud isiku kohta puudub HTML fail</h5>
-                    <?php endif; ?>
-                    <pre>
+                        <?php if (file_exists('results/' . $result['social_id'] . '.html')): ?>
+                            <button id="view-<?= $result['user_id'] ?>" class="preview"
+                                    data-target="#modal-<?= $result['user_id'] ?>"
+                                    data-toggle="modal">
+                                Eelvaade
+                            </button>
+                            <a href="results/<?= $result['social_id'] ?>.html" target="_blank">Link</a>
+                            <br>
+                            <br>
+                        <?php else: ?>
+                            <h5>Antud isiku kohta puudub HTML fail</h5>
+                        <?php endif; ?>
+                        <pre>
                             <?= htmlentities(file_get_contents('results/' . $result["social_id"] . '.html')); ?>
                         </pre>
-                    <?php if(!empty($result['practical_errors'])): ?>
-                    <h4>HTML errorid</h4>
-                    <?php if(empty(unserialize($result['practical_errors']))): ?>
-                    <h6>Document checking completed. No errors or warnings to show.</h6>
+                        <?php if (!empty($result['practical_errors'])): ?>
+                        <h4>HTML errorid</h4>
+                    <?php if (empty(unserialize($result['practical_errors']))): ?>
+                        <h6>Document checking completed. No errors or warnings to show.</h6>
                     <?php endif; ?>
-                    <ul>
-                        <?php foreach (unserialize($result['practical_errors']) as $error): ?>
-                            <li><?= $error; ?></li>
-                        <?php endforeach; ?>
-                        <?php endif; ?>
-                    </ul>
-                    <!-- grading -->
-                    <h4>Hindamine</h4>
+                        <ul>
+                            <?php foreach (unserialize($result['practical_errors']) as $error): ?>
+                                <li><?= $error; ?></li>
+                            <?php endforeach; ?>
+                            <?php endif; ?>
+                        </ul>
+                        <!-- grading -->
+                        <h4>Hindamine</h4>
 
 
-                    <div class="btn-group" data-toggle="pill">
-                    <?php for ($i = 0; $i <= 10; $i++): ?>
-                        <?php if ($result['practical_points'] == $i): ?>
-                            <label id="<?= $result['user_id'] ?>" class="btn active focus">
-                                <input type="radio" name="<?= $result['user_id'] ?>" value="<?= $i; ?>">
-                                <span><?= $i; ?></span>
-                            </label>
+                        <div class="btn-group" data-toggle="pill">
+                            <?php for ($i = 0; $i <= 10; $i++): ?>
+                                <?php if ($result['practical_points'] == $i): ?>
+                                    <label id="<?= $result['user_id'] ?>" class="btn active focus">
+                                        <input type="radio" name="<?= $result['user_id'] ?>" value="<?= $i; ?>">
+                                        <span><?= $i; ?></span>
+                                    </label>
+                                <?php else: ?>
+                                    <label id="<?= $result['user_id'] ?>" class="btn">
+                                        <input type="radio" name="<?= $result['user_id'] ?>" value="<?= $i; ?>">
+                                        <span><?= $i; ?></span>
+                                    </label>
+                                <?php endif ?>
+                            <?php endfor ?>
+                        </div>
+
+                        <?php if ($result['practical_points'] == -1): ?>
+                            <span class="bottom-not-graded-<?= $result['user_id'] ?> not-graded">Hindamata</span>
                         <?php else: ?>
-                        <label id="<?= $result['user_id'] ?>" class="btn">
-                            <input type="radio" name="<?= $result['user_id'] ?>" value="<?= $i; ?>">
-                            <span><?= $i; ?></span>
-                        </label>
-                        <?php endif ?>
-                    <?php endfor ?>
+                            <span class="graded-<?= $result['user_id'] ?> graded-green">Hinnatud</span>
+                        <?php endif; ?>
                     </div>
-
-                    <?php if ($result['practical_points'] == -1): ?>
-                        <span class="not-graded-<?= $result['user_id'] ?> not-graded">Hindamata</span>
-                    <?php else: ?>
-                        <span class="graded-<?= $result['user_id'] ?> graded-green">Hinnatud</span>
-                    <?php endif; ?>
-                </div>
                 <?php endif; ?>
-                <?php endforeach ?>
-            </div>
+            <?php endforeach ?>
         </div>
+    </div>
 
     </div>
 
-    <?php foreach ($results as $result): ?>
-        <?php if ($result['practical_points'] != -2): ?>
+    <?php if (file_exists('results/' . $result['social_id'] . '.html')): ?>
+        <?php foreach ($results as $result): ?>
+            <?php if ($result['practical_points'] != -2): ?>
 
-            <!-- PREVIEW MODALS -->
-            <div class="modal fade" id="modal-<?= $result['user_id'] ?>" role="dialog">
-                <div class="modal-dialog">
+                <!-- PREVIEW MODALS -->
+                <div class="modal fade" id="modal-<?= $result['user_id'] ?>" role="dialog">
+                    <div class="modal-dialog">
 
-                    <!-- Modal content-->
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal">&times;</button>
-                            <h4 class="modal-title"><?= $result['firstname'] . ' ' . $result['lastname'] . ', ' . $result['social_id'] ?></h4>
+                        <!-- Modal content-->
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                <h4 class="modal-title"><?= $result['firstname'] . ' ' . $result['lastname'] . ', ' . $result['social_id'] ?></h4>
+                            </div>
+                            <div class="modal-body">
+                                <iframe class="preview-modal"
+                                        src="results/<?= $result['social_id'] . '.html' ?>"></iframe>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Sulge</button>
+                            </div>
                         </div>
-                        <div class="modal-body">
-                            <iframe class="preview-modal" src="results/<?= $result['social_id'] . '.html' ?>"></iframe>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-default" data-dismiss="modal">Sulge</button>
-                        </div>
+
                     </div>
-
                 </div>
-            </div>
 
-        <?php endif; ?>
-    <?php endforeach ?>
+            <?php endif; ?>
+        <?php endforeach ?>
+    <?php endif; ?>
 
     <footer>
         <div class="col-md-4 footer-block">
@@ -156,8 +159,6 @@
 
         $(document).ready(function () {
 
-            var x = 0;
-
             // make the first items active
             $('ul#myTabs li:first').addClass('active');
             $('.tab-content .tab-pane:first').addClass('in active');
@@ -177,15 +178,12 @@
                 var id = this.id;
                 var value = $('.active>input[name=' + id + ']').val();
 
-                console.log(x + ". Grade: " + value + ", ID: " + id);
-
                 $.post('admin/gradePractical', {'user_id': id, 'grade': value},
                     function (res) {
                         if (res == 'ok') {
-                            x++;
-                            console.log(x + ". Grade: " + value + ", ID: " + id);
+                            $('.not-graded-' + id).removeClass('not-graded').addClass('graded').html('Hinnatud: "' + value + '"');
+                            $('.bottom-not-graded-' + id).removeClass('not-graded').addClass('graded-green').hide().html('Hinnatud').fadeIn('fast');
                             $('#graded-' + id).html('"' + value + '"');
-                            $('.graded-' + id).hide().html("Hinnatud").fadeIn('fast');
                         } else {
                             alert(res);
                         }
