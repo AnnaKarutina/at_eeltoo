219270e94f2bbd26ab59451c4909d5becbfbbdf3
reneesaks <renee.saks@khk.ee>
2017-03-16 10:56:36 +0200

    Removed unnecessary stylesheets from error template

--- templates/error_template.php
+++ templates/error_template.php
@@ -23,12 +23,8 @@ ___________              __           ____  __.___ ___  ____  __.
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-    <link href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css" rel="stylesheet">
-    <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">
     <link href="<?= BASE_URL ?>/assets/css/404.css" rel="stylesheet">
 </head>
-
 <body>
-
 </body>
 </html>
